69ba47f2d042b1549286d126178451cf407f8cf7
Francisco <panchoseur@gmail.com>
2021-03-30 11:44:14 -0300

    First commit

---
+++ src/main/java/com/everis/latam/ACLCorreoProveedoresFlama/AclCorreoProveedoresFlamaApplication.java
@@ -0,0 +1,13 @@
+package com.everis.latam.ACLCorreoProveedoresFlama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclCorreoProveedoresFlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclCorreoProveedoresFlamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/latam/ACLCorreoProveedoresFlama/AclCorreoProveedoresFlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.latam.ACLCorreoProveedoresFlama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclCorreoProveedoresFlamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
